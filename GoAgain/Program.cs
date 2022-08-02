93551b3841626ed746cafecf3f2126eec375a270
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-08-02 13:41:28 -0400

    Loops and going again

---
+++ GoAgain/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Instructions:
+Ask the user to enter a number
+Print out the square of the number
+Ask the user if they want to go again. They can enter y or Y to go again, n or N to quit
+*/
+
+string entry;
+bool keepGoing = true;
+
+
+do
+{
+    Console.Write("Please enter a number: ");
+    double num = double.Parse(Console.ReadLine());
+    System.Console.WriteLine($"That number squares is {num * num}");
+
+    // Ask the user if they wanna go again
+    bool valid = false;
+    do
+    {
+        Console.WriteLine("Want to play again? (y/n)");
+        entry = Console.ReadLine().ToLower();
+        if (entry == "n" || entry == "no") {
+            valid = true;
+            keepGoing = false;
+        }
+        else if (entry == "y" || entry == "yes") {
+            valid = true;
+        }
+    } while (valid == false);
+
+} while (keepGoing);
+
+
---
+++ LoopsDemo1/Program.cs
@@ -0,0 +1,74 @@
+﻿// For loop
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+for (int i = 20; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 21; i >= 0; i-=3)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("All done!");
+
+
+// While loop
+
+int w = 1;
+while (w < 5)
+{
+    Console.WriteLine("Inside while loop");
+    Console.WriteLine(w);
+    w++;
+}
+
+// Example of a break statement inside a while(true)
+
+int w2 = 10;
+while (w2 >= 0)
+{
+    if (w2 == 4) {
+        Console.WriteLine("Oops, found corrupted data");
+        break; // exits the LOOP immediately (doesn't exit "if")
+    }
+    Console.WriteLine(w2);
+    w2--;
+}
+
+Console.WriteLine("Finished with while loops");
+Console.WriteLine();
+
+int w3 = 0;
+do
+{
+    System.Console.WriteLine(w3);
+    w3++;
+} while (w3 < 10);
+
+Console.WriteLine("All done with do while!");
+Console.WriteLine();
+
+// Nested loops
+
+// for (int hour = 0; hour < 24; hour ++) {
+//     System.Console.WriteLine($"The hour is {hour}");
+//     for (int minute = 0; minute < 60; minute ++) {
+//         System.Console.WriteLine($"   {hour}:{minute}");
+//     }
+// }
+
+
+for (int i = 0; i < 12; i++) {
+    Console.WriteLine($"Hour is: {i}");
+    for (int j = 1; j < 60; j++) {
+        Console.WriteLine($"\t{i}:{j}");
+    }
+}
